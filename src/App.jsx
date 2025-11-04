8dd97102a08f79e2c93507b0beaabd2e7ead30fc
mdjunaidjewel <mdjunaidjewell@gmail.com>
2025-11-04 16:51:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css'
+
+function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = { name, email };
+
+    // Send to API or Server site data
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+                if (data.insertedId) {
+                  alert("Successfull Add the User in MongoDB");
+                  e.target.reset;
+                }
+    })
+
+ }
+
+  return (
+    <>
+
+      <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='User Name' /> <br />
+        <input type="email" name='email' placeholder='User Email' /> <br />
+        <input type="submit" value='Add User' placeholder='User Name' /> <br />
+      </form>
+    </>
+  )
+}
+
+export default App
