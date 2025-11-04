f0f3b3eec76efaa8aa4a51c6b0ca9924c640df03
mdjunaidjewel <mdjunaidjewell@gmail.com>
2025-11-04 17:49:52 +0600

    Add newUser add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 
+import { use, useState } from 'react';
 import './App.css'
 
+const userPromise = fetch("http://localhost:3000/users/").then(res => res.json());
+
 function App() {
+
+  const intialUsers = use(userPromise)
+  const [users, setUsers] = useState(intialUsers);
+
   const handleAddUser = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
@@ -19,7 +26,10 @@ function App() {
       .then(res => res.json())
       .then(data => {
         console.log(data)
-                if (data.insertedId) {
+        if (data.insertedId) {
+          newUser._id = data.insertedId;
+          const newUsers = [...users, newUser];
+          setUsers(newUsers)
                   alert("Successfull Add the User in MongoDB");
                   e.target.reset;
                 }
@@ -29,15 +39,20 @@ function App() {
 
   return (
     <>
-
-      <h1>Simple Crud</h1>
-      <form onSubmit={handleAddUser}>
-        <input type="text" name='name' placeholder='User Name' /> <br />
-        <input type="email" name='email' placeholder='User Email' /> <br />
-        <input type="submit" value='Add User' placeholder='User Name' /> <br />
-      </form>
+      <div>
+        <h1>Simple Crud</h1>
+        <form onSubmit={handleAddUser}>
+          <input type="text" name="name" placeholder="User Name" /> <br />
+          <input type="email" name="email" placeholder="User Email" /> <br />
+          <input type="submit" value="Add User" placeholder="User Name" />{" "}
+          <br />
+        </form>
+        {
+          users.map(user => <p key={user._id}>Name: {user.name} <br /> Email: { user.email}</p>)
+        }
+      </div>
     </>
-  )
+  );
 }
 
 export default App
